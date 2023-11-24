129699ec3a0b2173ad53a922a3d894251dd3c6dc
tarasmaliarchuk <tarasmalarcuk8@gmail.com>
2023-11-24 11:29:23 +0200

    add random color changing

--- src/components/Task2/Task2.jsx
+++ src/components/Task2/Task2.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Task2.css';
 
-const Cell = ({ id, onHover }) => {
+const Cell = ({ id, onHover, onHoverOut, onClick, columnColors, hoveredColor }) => {
     const onMouseEnterHandler = () => onHover(id);
+    const onMouseLeaveHandler = () => onHoverOut(id);
+    const onClickHandler = () => onClick(id);
+
+    const column = (id - 1) % 6;
+    const isColumnColored = columnColors[column];
+    const style = {
+        backgroundColor: id === 8 && hoveredColor ? hoveredColor : isColumnColored ? isColumnColored : undefined,
+    };
+
     return (
         <div
             id={`cell-${id}`}
             className="grid-item"
             onMouseEnter={onMouseEnterHandler}
+            onMouseLeave={onMouseLeaveHandler}
+            onClick={onClickHandler}
+            style={style}
         >
             {id}
         </div>
@@ -16,18 +28,60 @@ const Cell = ({ id, onHover }) => {
 
 
 const Task2 = () => {
-    let counter = 1;
-    const cells = Array(6)
-        .fill(null)
-        .map(() => Array(6).fill(null).map(() => counter++));
-    const onHover = () => {};
+    const [hoveredCell, setHoveredCell] = useState(null);
+    const [coloredColumns, setColoredColumns] = useState({});
+
+    const onHover = (id) => {
+        if (id === 8) {
+            setHoveredCell(getRandomColor());
+        }
+    };
+
+    const onHoverOut = () => {
+        setHoveredCell(null);
+    };
+
+    const onClick = (id) => {
+        const clickedColumn = (id - 1) % 6;
+        const newColoredColumns = { ...coloredColumns };
+
+        let shouldColor = !!newColoredColumns[clickedColumn];
+        for (let i = clickedColumn; i < 6; i += 2) {
+            if (shouldColor) {
+                delete newColoredColumns[i];
+            } else {
+                newColoredColumns[i] = getRandomColor();
+            }
+        }
+        setColoredColumns(newColoredColumns);
+    };
+
+    const getRandomColor = () => {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    const cells = Array.from({ length: 36 }, (_, index) => index + 1);
     return (
         <div className="grid-container">
-            {
-                cells.map((row, rowIndex) =>
-                    row.map((id) => <Cell key={`cell-${id}`} id={id} onHover={onHover} />)
-                )
-            }
+            {cells.map((id) => {
+                const column = (id - 1) % 6;
+                return (
+                    <Cell
+                        key={`cell-${id}`}
+                        id={id}
+                        onHover={onHover}
+                        onHoverOut={onHoverOut}
+                        onClick={onClick}
+                        columnColors={coloredColumns}
+                        hoveredColor={hoveredCell && id === 8 ? hoveredCell : null}
+                    />
+                );
+            })}
         </div>
     );
 };
